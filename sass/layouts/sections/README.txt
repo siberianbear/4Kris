e7be9e8a646ee28159d7424f01395d59b52a001e
Kris Gunciarz <kris@waat.eu>
2016-01-06 10:56:41 +0100

    replacing grunt with gulp and standard kss styleguide with sc5 styleguide

--- sass/layouts/sections/README.txt
+++
@@ -1,7 +0,0 @@
-This folder is for custom layout modifications that cannot be related to any component.
-Mainly stylings like margins, paddings and backgrounds.
-
-Each section class should have separated scss file here, added to app.scss aggegation file.
-Naming convention is: section--section-name
-
-Let section--header guide you as an example.
--- styleguide/assets/js/components/news-carousel/news-carousel.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  $('#myCarousel').on('slid.bs.carousel', function () {
-    var currentIndex = $('#myCarousel .carousel-inner .active').index('#myCarousel .item');
-    $('#myCarousel .list-group-item.active').removeClass('active');
-    $("#myCarousel .list-group-item[data-slide-to='" + currentIndex + "']").addClass('active');
-  })
-})
